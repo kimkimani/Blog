4b0e38f580648a7427d21ba22592020621576872
kim <kimkimani016@gmail.com>
2020-02-10 13:08:25 +0300

    Initial commit

---
+++ app/src/main/java/free/blog/myapplication/RegesterActivity.java
@@ -0,0 +1,14 @@
+package free.blog.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regester);
+    }
+}
